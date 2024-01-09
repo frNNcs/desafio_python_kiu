78667d850ed0655260a62facd46407b02fb4c5e7
francisco <frnncs@gmail.com>
2024-01-09 20:46:16 -0300

    add init db at database connection module

--- src/project/database/connection.py
+++ src/project/database/connection.py
@@ -1,5 +1,8 @@
 import psycopg2
 
 from project.config.base import CONNECTION_STRING
+from project.database.init_db import init_db
 
 conn = psycopg2.connect(CONNECTION_STRING)
+
+init_db()
--- src/project/database/init_db.py
+++ src/project/database/init_db.py
@@ -1,16 +1,16 @@
-#!/bin/bash
-set -e
+def init_db():
+    from .connection import conn as connection
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE clients (
-            id serial PRIMARY KEY,
-            created_at timestamp,
-            name varchar(100) NOT NULL,
-            email varchar(100) NOT NULL,
-            phone varchar(100) NOT NULL,
-            address varchar(100) NOT NULL,
-            is_active boolean NOT NULL
-        );
+    SQL = """
+        CREATE TABLE IF NOT EXISTS clients (
+                id serial PRIMARY KEY,
+                created_at timestamp,
+                name varchar(100) NOT NULL,
+                email varchar(100) NOT NULL,
+                phone varchar(100) NOT NULL,
+                address varchar(100) NOT NULL,
+                is_active boolean NOT NULL
+            );
         CREATE TABLE IF NOT EXISTS packages (
             id serial PRIMARY KEY,
             created_at timestamp,
@@ -19,7 +19,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
             weight float NOT NULL,
             size varchar(100) NOT NULL
         );
-        CREATE TABLE shipments (
+        CREATE TABLE IF NOT EXISTS shipments (
             id serial PRIMARY KEY,
             created_at timestamp,
             source_id integer NOT NULL REFERENCES clients(id),
@@ -28,4 +28,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
             state varchar(100) NOT NULL,
             package_id integer NOT NULL REFERENCES packages(id)
         );
-EOSQL
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(SQL)
+        connection.commit()
+
+
+if __name__ == "__main__":
+    init_db()
