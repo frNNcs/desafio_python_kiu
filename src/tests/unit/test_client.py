40bbd8bd59fa9b074379913da8fc285773e62835
francisco <frnncs@gmail.com>
2024-01-09 14:06:10 -0300

    add 2 new test, one before it passes, (TDD)

--- src/tests/unit/test_client.py
+++ src/tests/unit/test_client.py
@@ -72,6 +72,36 @@ class TestClient(unittest.TestCase):
             )
         self.assertTrue("Client is not a valid client", str(context.exception))
 
+    def test_client_canot_send_package_to_itself(self):
+        """Test that the client can't send a package to itself."""
+
+        with self.assertRaises(Exception) as context:
+            self.client.send_package(
+                destination=self.client,
+                package=Package(
+                    description="Test package",
+                    type_package="Test type",
+                    weight=10.0,
+                    size=(10, 10, 10),
+                ),
+            )
+        self.assertTrue("Can't send a package to itself", str(context.exception))
+
+    def test_get_client_dict(self):
+        """Test that the client can be converted to a dict"""
+        self.assertEqual(
+            self.client.__dict__(),
+            {
+                "id": self.client.id,
+                "created_at": self.client.created_at.strftime("%d-%m-%Y %H:%M:%S"),
+                "name": "Test Client",
+                "email": "pablito@asdasd.com",
+                "phone": "123123123",
+                "address": "Test Address",
+                "is_active": True,
+            },
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
